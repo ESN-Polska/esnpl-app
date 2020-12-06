c17d679767781fb9f63284b8f391d9ec823ac0d4
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-12-06 11:42:49 +0100

    Add documents page (#8)
    * Add backend connection to documents page. Add init version of page content
    
    * Add key property to Agenda
    
    * Minor refacotring and improvements

--- src/pages/documents/Documents.tsx
+++ src/pages/documents/Documents.tsx
@@ -1,7 +1,40 @@
-import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import { IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonContent, IonList } from "@ionic/react";
+import React, { useState, useEffect } from "react";
+
+import API from "../../utils/backend";
+
+interface DocumentObject {
+  name: string;
+  url: string;
+  key: string;
+}
 
 function Documents() {
+  const [documentsData, setdocumentsData] = useState([]);
+
+  useEffect(() => {
+    //TODO: add feature to refresh data
+    API.getDocuments()
+      .then(({ data: { values } }: { data: { values: string[] } }) =>
+        values.map((rawRow: any, index: number) => ({
+          name: rawRow[0],
+          url: rawRow[1],
+          key: `doc-item-${index}`,
+        }))
+      )
+      .then((data: any) => {
+        setdocumentsData(data);
+      });
+  }, []);
+
+  const DocumentsItem = ({ documentObject }: { documentObject: DocumentObject }) => (
+    <IonItem onClick={() => window.open(documentObject.url)}>
+      <IonLabel>
+        <h3>{documentObject.name}</h3>
+      </IonLabel>
+    </IonItem>
+  );
+
   return (
     <IonPage>
       <IonHeader>
@@ -9,6 +42,13 @@ function Documents() {
           <IonTitle>Documents</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonContent>
+        <IonList>
+          {documentsData.map((entry: DocumentObject) => (
+            <DocumentsItem documentObject={entry} key={entry.key} />
+          ))}
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 }
