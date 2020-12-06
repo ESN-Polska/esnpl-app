21ba274d5f66c919c6d43be8e97195ebe988feb0
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-12-07 00:12:41 +0100

    Add button with edit version of speakers list

--- src/pages/speakers-list/SpeakersList.tsx
+++ src/pages/speakers-list/SpeakersList.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import React from "react";
 
 //TODO: use real id
@@ -6,6 +6,7 @@ const gId = "2PACX-1vTPE6tVWFXpJp21mGb9q99dXoMpMrWqskNvXu8rJrzpieVNexFB5tcbcyRKV
 const sheetId = "944840956";
 
 const speakersListUrl = `https://docs.google.com/spreadsheets/d/e/${gId}/pubhtml?gid=${sheetId}&range=a1:s45&single=true&widget=true&headers=false`;
+const editSpeakersListUrl = `https://docs.google.com/spreadsheets/d/1vlblH7pR2kxhAQuqWCAXCTbFj0CqiQtwC8z_zOsQ5FM/edit#gid=944840956`;
 
 function SpeakersList() {
   return (
@@ -16,7 +17,8 @@ function SpeakersList() {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <iframe src={speakersListUrl} width="100%" height="100%" title="Speakers List"></iframe>
+        <iframe src={speakersListUrl} width="100%" height="90%" title="Speakers List"></iframe>
+        <IonButton onClick={() => window.open(editSpeakersListUrl)}> Sign up for the speakers list</IonButton>
       </IonContent>
     </IonPage>
   );
