52e95c458fedbdfae6b6b2bd838bc9a984f3cadc
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-12-06 01:38:26 +0100

    Improvements general (#7)
    * Remove map page
    
    * Agenda component refactor
    
    * add divider with name of a day
    
    * Minor refactoring
    
    * Update icons. App name etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import { IonApp, IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs, IonRouterOutlet } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import {
-  calendarOutline as calendarIcon,
-  documentAttachOutline as docIcon,
-  mapOutline as mapIcon,
-  peopleOutline as peopleIcon,
-  wifiOutline as wifiIcon,
-} from "ionicons/icons";
+import { calendarOutline as calendarIcon, documentAttachOutline as docIcon, peopleOutline as peopleIcon, wifiOutline as wifiIcon } from "ionicons/icons";
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 import Agenda from "./pages/agenda/Agenda";
 import Documents from "./pages/documents/Documents";
-import Map from "./pages/map/Map";
 import OC from "./pages/oc/OC";
 import Stream from "./pages/stream/Stream";
 
@@ -26,7 +19,6 @@ const App: React.FC = () => {
             <Route path="/stream" component={Stream} exact={true} />
             <Route path="/documents" component={Documents} exact={true} />
             <Route path="/OC" component={OC} exact={true} />
-            <Route path="/map" component={Map} exact={true} />
             <Route path="/" render={() => <Redirect to="/agenda" />} exact={true} />
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
@@ -45,10 +37,6 @@ const App: React.FC = () => {
             <IonTabButton tab="oc" href="/OC">
               <IonIcon icon={peopleIcon} />
               <IonLabel>OC</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="map" href="/map">
-              <IonIcon icon={mapIcon} />
-              <IonLabel>Map</IonLabel>
             </IonTabButton>
           </IonTabBar>
         </IonTabs>
--- src/pages/agenda/Agenda.tsx
+++ src/pages/agenda/Agenda.tsx
@@ -4,16 +4,31 @@ import "./Agenda.scss";
 
 import API from "../../utils/backend";
 
-interface AgendaItem {
+interface AgendaEntry {
   startTime: string;
   duration: string;
   topic: string;
-  type: string; //todo enum
+  type: string; //TODO: change to enum
   speaker: string;
-  day: string; //todo: enum
+  day: string; //TODO: change to enum
 }
 
-const createAgendaItem = (rawEntry: any): AgendaItem | undefined => {
+enum dayName {
+  FRI = "Friday",
+  SAT = "Saturday",
+  SUN = "Sunday",
+}
+
+//TODO: improve after 8hrs of good sleep....
+const getDayName = (name: string) => {
+  if (name === "FRI") return dayName.FRI;
+
+  if (name === "SAT") return dayName.SAT;
+
+  if (name === "SUN") return dayName.SUN;
+};
+
+const createAgendaItem = (rawEntry: any): AgendaEntry | undefined => {
   if (!(rawEntry[2] && rawEntry[3] && rawEntry[4] && rawEntry[5] && rawEntry[6])) return;
 
   return {
@@ -26,22 +41,59 @@ const createAgendaItem = (rawEntry: any): AgendaItem | undefined => {
   };
 };
 
+const AgendaItemsList = ({ agendaData }: { agendaData: AgendaEntry[] }) => (
+  <IonList>
+    {agendaData.map((item: AgendaEntry | string) => {
+      if (!item) return undefined;
+
+      return typeof item === "string" ? <AgendaDividerItem dayName={item} /> : <AgendaEntryItem itemData={item} />;
+    })}
+  </IonList>
+);
+
+const AgendaEntryItem = ({ itemData }: { itemData: AgendaEntry }) => (
+  <IonItem>
+    <IonLabel>
+      <div className="agenda-item">
+        <div className="time-slot">
+          <p className="secondary-data-field">{itemData.day}</p>
+          <h2 className="primary-data-field">{itemData.startTime}</h2>
+          <p className="secondary-data-field">{itemData.duration}</p>
+        </div>
+        <div className="rest-slot">
+          <h3 className="topic-field">{itemData.topic}</h3>
+          <p className="speaker-field">Speaker: {itemData.speaker} </p>
+        </div>
+      </div>
+    </IonLabel>
+  </IonItem>
+);
+
+const AgendaDividerItem = ({ dayName }: { dayName: string }) => (
+  <IonItem>
+    <IonLabel>
+      <h1>{dayName}</h1>
+    </IonLabel>
+  </IonItem>
+);
+
 function Agenda() {
   const [agendaData, setAgendaData] = useState([]);
 
   useEffect(() => {
     let agendaSumData: any = [];
 
+    //TODO: add feature to refresh agenda
     API.getAgenda()
       .then((dataArr: { data: any }[]) =>
         dataArr.map(
           (arr) =>
             (agendaSumData = [
               ...agendaSumData,
+              getDayName(arr.data.values[1][11]),
               ...arr.data.values
                 .map((rawItem: string[]) => {
                   if (rawItem[2] === "PLANNED") return undefined;
-
                   return createAgendaItem(rawItem);
                 })
                 .filter((value: any) => value !== undefined),
@@ -53,27 +105,6 @@ function Agenda() {
       });
   }, []);
 
-  const agendaItems = agendaData.map(
-    (item: AgendaItem | undefined) =>
-      item && (
-        <IonItem>
-          <IonLabel>
-            <div className="agenda-item">
-              <div className="time-slot">
-                <p className="secondary-data-field">{item.day}</p>
-                <h2 className="primary-data-field">{item.startTime}</h2>
-                <p className="secondary-data-field">{item.duration}</p>
-              </div>
-              <div className="rest-slot">
-                <h3 className="topic-field">{item.topic}</h3>
-                <p className="speaker-field">Speaker: {item.speaker} </p>
-              </div>
-            </div>
-          </IonLabel>
-        </IonItem>
-      )
-  );
-
   return (
     <IonPage>
       <IonHeader>
@@ -82,7 +113,7 @@ function Agenda() {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonList>{agendaItems}</IonList>
+        <AgendaItemsList agendaData={agendaData} />
       </IonContent>
     </IonPage>
   );
