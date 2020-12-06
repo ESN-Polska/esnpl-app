bfe8988770ec6b86043232e4534e43b0ddd14a03
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-12-06 11:14:10 +0100

    Add backend connection to documents page. Add init version of page content

--- src/utils/backend.ts
+++ src/utils/backend.ts
@@ -1,18 +1,29 @@
 import axios from "axios";
 
 const API_KEY = "AIzaSyCdgPjocxnW-osbDmJsBoCiB5uVUty79FU";
-const DOC_ID = "1NmsiN24sdP-Q4JS75mpPZ1EZGuaLf8plgoCHLwpUaZI";
+const AGENDA_SHEETS_ID = "1NmsiN24sdP-Q4JS75mpPZ1EZGuaLf8plgoCHLwpUaZI";
+
+const BE_SHEETS_ID = "1vlblH7pR2kxhAQuqWCAXCTbFj0CqiQtwC8z_zOsQ5FM";
 const baseGoogleSheetsURL = "https://sheets.googleapis.com/v4/spreadsheets";
 
-const agendaUrl = (sheetId: string): string => {
-  return `${baseGoogleSheetsURL}/${DOC_ID}/values/'${sheetId}'!A1:L100?key=${API_KEY}`;
+const createAgendaUrl = (sheetId: string): string => {
+  return `${baseGoogleSheetsURL}/${AGENDA_SHEETS_ID}/values/'${sheetId}'!A1:L100?key=${API_KEY}`;
+};
+
+const createBeUrl = (sheetId: string): string => {
+  return `${baseGoogleSheetsURL}/${BE_SHEETS_ID}/values/'${sheetId}'!A1:L100?key=${API_KEY}`;
 };
 
 const getAgenda = (): any => {
   const sheetIds = ["Piątek (11.12)", "Sobota (12.12)", "Niedziela (13.12)"];
-  return Promise.all(sheetIds.map((sheetId: string) => axios.get(agendaUrl(sheetId))));
+  return Promise.all(sheetIds.map((sheetId: string) => axios.get(createAgendaUrl(sheetId))));
 };
 
-export const API = { getAgenda };
+const getDocuments = (): any => {
+  const documentsKey = "Documents";
+  return axios.get(createBeUrl(documentsKey));
+};
+
+export const API = { getAgenda, getDocuments };
 
 export default API;
